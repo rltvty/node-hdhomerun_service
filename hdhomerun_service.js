d0a3e387890babe5ba8e8fc773d2acf248181a04
Eric Pinzur <ericpinzur@otherinbox.com>
2014-11-28 16:27:24 -0500

    added device discovery

--- hdhomerun_service.js
+++ hdhomerun_service.js
@@ -1,13 +1,16 @@
 var http = require('http');
 var globals = require('./lib/globals');
 var express_app = require('./lib/express_app');
+var hdhr = require('./lib/hd_wrapper');
 
 var server = null;
+
 
 globals.logger.info('Running on node version: ' + process.version);
 
 module.exports.start = function() {
   if (server == null) {
+    hdhr.discover();
     server = http.createServer(express_app());
     server.listen(globals.config.http_server_port);
     globals.logger.info('Server listening on port '+ globals.config.http_server_port);
@@ -23,3 +26,5 @@ module.exports.stop = function() {
 };
 
 module.exports.start();
+
+
