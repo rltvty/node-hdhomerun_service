5c31b5ce47deaa16f9e9be18dc08b278ec9d07f2
Eric Pinzur <ericpinzur@otherinbox.com>
2014-11-28 22:14:50 -0500

    added insertion of one local route

--- hdhomerun_service.js
+++ hdhomerun_service.js
@@ -2,5 +2,13 @@ var http = require('http');
 var rest_base = require('rest_base');
 var hdhr = require('./lib/hd_wrapper');
 
+function setup_routes(app) {
+    app.get('/pingy', function(request, response){
+        response.status(200).send('pongy');
+    });
+};
+
+rest_base.set_setup_routes_method(setup_routes);
 rest_base.start_server();
 hdhr.discover();
+
