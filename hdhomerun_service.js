16d4c6df93211e7dba14c41bd53e18e58fdff02d
Eric Pinzur <ericpinzur@otherinbox.com>
2014-11-28 21:33:35 -0500

    updated to use rest_base

--- hdhomerun_service.js
+++ hdhomerun_service.js
@@ -1,30 +1,6 @@
 var http = require('http');
-var globals = require('./lib/globals');
-var express_app = require('./lib/express_app');
+var rest_base = require('rest_base');
 var hdhr = require('./lib/hd_wrapper');
 
-var server = null;
-
-
-globals.logger.info('Running on node version: ' + process.version);
-
-module.exports.start = function() {
-  if (server == null) {
-    hdhr.discover();
-    server = http.createServer(express_app());
-    server.listen(globals.config.http_server_port);
-    globals.logger.info('Server listening on port '+ globals.config.http_server_port);
-  }
-};
-
-module.exports.stop = function() {
-  if (server != null) {
-    server.close();
-    server = null;
-    globals.logger.info('Server stopped listening on port '+ globals.config.http_server_port);
-  }
-};
-
-module.exports.start();
-
-
+rest_base.start_server();
+hdhr.discover();
